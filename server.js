f0ffb6b0e4b294e1a462177d8f41f83fe54583d4
unknown <novagreen53000@gmail.com>
2026-02-27 08:43:21 +0100

    fix scroll clean block

--- server.js
+++ server.js
@@ -311,4 +311,8 @@ const reveals=document.querySelectorAll(".reveal");
 window.addEventListener("scroll",()=>{
 reveals.forEach(section=>{
 const top=section.getBoundingClientRect().top;
-if(top<window.
+if(top<window.innerHeight-100){
+section.classList.add("visible");
+}
+});
+});
